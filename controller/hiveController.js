760d2916370778cf5ee7659cd97aa9f14316288d
JernejL123 <jernej.lobnik@gmail.com>
2026-01-15 22:34:35 +0100

    bugFix dodan req.querry

--- controller/hiveController.js
+++ controller/hiveController.js
@@ -131,7 +131,7 @@ createAdmin: async function (req, res) {
   },
 
   remove:function(req,res){
-    const hiveId = req.params.id ?? req.body.id;  
+    const hiveId = req.params.id ?? req.body.id ?? req.query.id;  
     const userId = req.auth.data.id
 
     console.log("hiveid", hiveId)
