de987c28db006cc18eee93fe59ca9abeddd76458
Nejc Sorgo <nejc.sorgo@gmail.com>
2026-01-18 18:52:53 +0100

    BA-82 added query params to remove 2

--- controller/notesController.js
+++ controller/notesController.js
@@ -106,9 +106,9 @@ const NotesController = {
   },
 
   async remove(req, res) {
-    const userId = req.auth.data.id ?? req.query.id; // TOKEN -> JWT
+    const userId = req.auth.data.id ; // TOKEN -> JWT
     //const hiveId = req.params.id ?? 
-    const noteId = req.body.id; // 
+    const noteId = req.body.id ?? req.query.id; // 
 
     console.log("UserId", userId);
     console.log("notesId",noteId);
